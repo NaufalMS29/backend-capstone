44f3c01acee97d0600b393331f66fe6b064d4b67
NaufalMS29 <naufalms2005@gmail.com>
2026-06-04 00:39:45 +0700

    edit middlewares

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,14 +2,21 @@ import response from '../utils/response.js';
 import { ClientError } from '../exceptions/index.js';
 
 const errorMiddleware = (err, req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+
   if (err instanceof ClientError) {
     return response(res, err.statusCode, err.message, null);
   }
+
   if (err.isJoi) {
     return response(res, 400, err.details[0].message, null);
   }
+
   const status = err.statusCode || err.status || 500;
   const message = err.message || 'Internal Server Error';
+
   console.error('Unhandled error:', err);
   return response(res, status, message, null);
 };
--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,18 @@ import ErrorHandler from '../middlewares/error.js';
 const app = express();
 
 app.use(cors());
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.sendStatus(200);
+});
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use(routes);
 app.use(ErrorHandler);
 
