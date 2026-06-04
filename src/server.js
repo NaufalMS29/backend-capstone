adb5473b1f4ca33d0077bcb41e208437db0352b5
NaufalMS29 <naufalms2005@gmail.com>
2026-06-04 09:45:45 +0700

    edit server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import server from './server/index.js';
 
-if (process.env.NODE_ENV !== 'production') {
-  const port = process.env.PORT || 5000;
-  const host = process.env.HOST || 'localhost';
+const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV !== 'production' && process.env.VERCEL !== '1') {
+  const host = process.env.HOST || 'localhost';
   server.listen(port, () => {
     console.log(`🚀 Gateway Server running at http://${host}:${port}`);
   });
