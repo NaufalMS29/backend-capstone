4f13ca4116ad22f316fa51b094d8e9dda0839cc9
NaufalMS29 <naufalms2005@gmail.com>
2026-06-04 10:03:30 +0700

    edit server

--- src/server/index.js
+++ src/server/index.js
@@ -6,14 +6,24 @@ import ErrorHandler from '../middlewares/error.js';
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+];
+
 app.use(cors({
   origin: function (origin, callback) {
-    callback(null, true);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin || '*');
+    } else {
+      callback(new Error(`CORS: Origin ${origin} tidak diizinkan`));
+    }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-  credentials: true
+  credentials: true,
 }));
+
+app.options('*', cors());
 
 app.use(express.json());
 app.use(routes);
